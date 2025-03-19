1874aaf51e8bf119297ae97289966aa12d5ce5b2
yellouz <youssefellouz303@gmail.com>
2025-03-19 16:27:17 +0000

    upload

---
+++ exercice1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void PrintNumbers(int from, int to)
+{
+    for (int i = from; i <= to; i++)
+    {
+        printf("%d ", i);
+    }
+    printf("\n");
+}
+
+void ChildProcessPrintsNumbers(__pid_t ForkResult, int ProcessNumber, int from, int to)
+{
+    switch (ForkResult)
+    {
+    case -1:
+        exit(1);
+    
+    case 0:
+        printf("\nChild Process Number %d: (PID = %d, PPID = %d)\n", ProcessNumber, getpid(), getppid());
+        PrintNumbers(from, to);
+        exit(0);
+
+    default:
+        wait(0);
+        break;
+    }
+}
+
+int main()
+{
+    __pid_t pid;
+    int ProcessNumber = 0;
+
+    pid = fork();
+    ProcessNumber = 1;
+    ChildProcessPrintsNumbers(pid, ProcessNumber, 1, 50);
+
+    pid = fork();
+    ProcessNumber = 2;
+    ChildProcessPrintsNumbers(pid, ProcessNumber, 51, 100);
+}
